3b41b4bea2aa67847bb335c1911355639e1008c5
Huy Manh <huymanhtmhp@gmail.com>
2020-09-06 11:32:43 +0700

    Add navigation done

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
+import 'react-native-gesture-handler'
 import React from 'react'
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import { MainScreen } from './src/screens'
+
+const SettingScreen = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'blue',
+    }}
+    >
+      <Text>Setting Screen</Text>
+    </View>
+  )
+}
+
+const Stack = createStackNavigator()
 
 const App = () => {
   return (
-    <MainScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="MainScreen" component={MainScreen} />
+        <Stack.Screen name="SettingScreen" component={SettingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
